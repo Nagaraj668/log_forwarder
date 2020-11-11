bdccfe0fe89a2631d05db86ef8ccb203df374b71
Nagaraj N <Nagaraj668@users.noreply.github.com>
2020-11-11 12:47:12 +0530

    Initial Commit

---
+++ app_schedule.py
@@ -0,0 +1,43 @@
+import time
+import requests
+
+file_path = "./data/sample_logs.log"
+older_contents = ""
+
+# in seconds
+LOG_FORWARDER_INTERVAL = 5
+
+
+def read_for_older_contents():
+    global older_contents
+    older_contents = (open(file_path, "r")).read()
+
+
+def read_new_logs():
+    global older_contents
+    file = open(file_path, "r")
+    new_contents = file.read()
+    original_content = new_contents
+    new_contents = new_contents.replace(older_contents, "")
+    older_contents = original_content
+
+    # check for new logs and forward to splunk
+    if new_contents != "":
+        print("sending logs")
+        send_new_contents(new_contents)
+
+    file.close()
+
+
+def send_new_contents(new_contents):
+    url = 'http://www.w3schools.com/python/demopage.php'
+    data_to_splunk = {'logs': new_contents}
+    req_object = requests.post(url, data=data_to_splunk)
+    print(req_object.text)
+
+
+read_for_older_contents()
+
+while True:
+    read_new_logs()
+    time.sleep(LOG_FORWARDER_INTERVAL)
---
+++ watchdog_monitor.py
@@ -0,0 +1,48 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+import os
+
+if __name__ == "__main__":
+    patterns = "*"
+    ignore_patterns = ""
+    ignore_directories = False
+    case_sensitive = True
+    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+
+
+def on_created(event):
+    print(f"{event.src_path} has been created")
+    os.system(f"git add {event.src_path}")
+    os.system(f"git commit -m '{event.src_path} created'")
+    os.system("git push origin master")
+
+
+def on_deleted(event):
+    print(f"Delete {event.src_path}!")
+    os.system(f"git commit -m '{event.src_path} deleted'")
+    os.system("git push origin master")
+
+
+def on_modified(event):
+    print(f"{event.src_path} has been modified")
+    os.system(f"git commit -m '{event.src_path} modified'")
+    os.system("git push origin master")
+
+
+my_event_handler.on_created = on_created
+my_event_handler.on_deleted = on_deleted
+my_event_handler.on_modified = on_modified
+
+path = "D:\\Projects\\Python\\log_forwarder\\data\\"
+go_recursively = True
+my_observer = Observer()
+my_observer.schedule(my_event_handler, path, recursive=go_recursively)
+
+my_observer.start()
+try:
+    while True:
+        time.sleep(5000)
+except:
+    my_observer.stop()
+my_observer.join()
